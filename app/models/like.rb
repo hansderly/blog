b51b3e906b8bea55fb1afcb0a60a189780df020c
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-04 15:18:20 -0400

    Add method  update like for like class

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save: :update_post_litkes_counter
+
+  private
+
+  def udpate_post_likes_counter
+    post.increment!(:likes_count)
+  end
 end
