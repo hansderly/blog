f6e88e031e8a210415b9b09250f7a8ebe3a07a32
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-04 18:14:23 -0400

    Fix typo in like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_post_litkes_counter
+  after_save :update_post_likes_counter
 
   private
 
-  def udpate_post_likes_counter
-    post.increment!(:likes_count)
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
   end
 end
