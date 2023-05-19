36b7ee8471fdc523930159fecb7076767d918fb9
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-19 14:21:23 -0600

    Define create method for comments controller to add a new comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,4 +6,16 @@ class Api::V1::CommentsController < Api::V1::MainController
     render json: @post.comments
   end
 
+  def create
+    auth_token = request.headers['Authorization']
+    current_user = User.find_by(user_token: auth_token)
+    @new_comment = Comment.new(params.require(:comment).permit(:text))
+    @new_comment.author = current_user
+    @new_comment.post = Post.find(params[:post_id])
+    if @new_comment.save
+      render json: @new_comment, status: :created
+    else
+      render json: @new_comment.errors, status: :unprocessable_entity
+    end
+  end
 end
