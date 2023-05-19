65db6f19cc2efe9a24a32420cb59f0c951071cb7
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-19 15:30:26 -0400

    Add comments_controller.rb file ewith index method

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::CommentsController < Api::V1::MainController
+  skip_before_action :authenticate, only: :index
+
+  def index
+    @post = Post.includes(:comments).find(params[:post_id])
+    render json: @post.comments
+  end
+
+
+end
